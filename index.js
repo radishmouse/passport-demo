9d7b53c0c4b49eced008e2f7401192593bdf0429
Chris Aquino <chris.aquino@gmail.com>
2019-04-09 10:55:25 -0400

    completes setup of github oauth

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const setupAuth = require('./auth');
+
+// tell express to use the session modules
+app.use(session({
+    store: new FileStore(),  // no options for now
+    secret: process.env.SESSION_SECRET
+}));
+
+// Only after we have sessions set up with express
+// is it ok to attach the passport authentication
+setupAuth(app);
+
 app.get('/', (req, res) => {
     res.send(`
         <h1>Hello There</h1>    
